a50b52245451a7762563475f0b28be1c1cc459b7
balY-S <121259489+balY-S@users.noreply.github.com>
2022-12-22 23:08:13 +0300

    Add files via upload

---
+++ lab10/lab13/SortingStudentsByGPA.java
@@ -0,0 +1,10 @@
+package ru.mirea.lab13;
+
+import java.util.Comparator;
+
+public class SortingStudentsByGPA implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        return ((Student) o1).getGPA() - ((Student) o2).getGPA();
+    }
+}
