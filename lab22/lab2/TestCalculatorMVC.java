b9f144db90bb660a741159c9dbf9afbca1393cf7
balY-S <121259489+balY-S@users.noreply.github.com>
2022-12-22 23:12:51 +0300

    Add files via upload

---
+++ lab22/lab2/TestCalculatorMVC.java
@@ -0,0 +1,9 @@
+package ru.mirea.lab2;
+
+public class TestCalculatorMVC {
+    public static void main(String[] args) {
+        CalculatorModel model = new CalculatorModel();
+        CalculatorView view = new CalculatorView();
+        CalculatorController controller = new CalculatorController(model, view);
+    }
+}
